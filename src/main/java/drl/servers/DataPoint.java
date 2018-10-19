a9d0ac0da822520be437a54b42a8522190eb7eeb
Trevor Morton <mortontc@rose-hulman.edu>
2018-10-19 13:26:50 -0400

    Fixed terminal state logic

--- src/main/java/drl/servers/DataPoint.java
+++ src/main/java/drl/servers/DataPoint.java
@@ -5,7 +5,7 @@ import org.nd4j.linalg.dataset.MultiDataSet;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.conditions.Condition;
 
-import static org.nd4j.linalg.ops.transforms.Transforms.abs;
+import static org.nd4j.linalg.ops.transforms.Transforms.*;
 
 public class DataPoint {
     INDArray[] startState;
@@ -24,18 +24,17 @@ public class DataPoint {
         INDArray[] qOffsetLabels = new INDArray[this.labels.length];
 
         INDArray max = currentLabels[0];
+        INDArray terminal = abs(currentLabels[0]);
         for(int i = 1; i < currentLabels.length; i++){
             INDArray cur = currentLabels[i];
             max = max.add(cur).add(abs(max.sub(cur))).mul(1 / 2);
+            terminal = terminal.add(abs(cur)).add(abs(terminal.sub(abs(cur)))).mul(1 / 2);
         }
 
+        terminal = not(greaterThanOrEqual(terminal, Nd4j.ones()));
+
         for(int i = 0; i < this.labels.length; i++){
-            if(this.labels[i].amaxNumber().intValue() == 1){
-                qOffsetLabels[i] = this.labels[i];
-            }
-            else {
-                qOffsetLabels[i] = this.labels[i].add(max);
-            }
+            qOffsetLabels[i] = this.labels[i].add(max.mul(terminal));
         }
 
         return new MultiDataSet(this.startState, qOffsetLabels, null, masks);
