5ff633885bc3ad26b808c0a30543094e7891f06a
Trevor Morton <mortontc@rose-hulman.edu>
2018-11-20 13:40:57 -0500

    Removed jpy build

--- jpy-build/lib.linux-x86_64-3.6/jpyconfig.py
+++
@@ -1,8 +0,0 @@
-# Created by 'jpyutil.py' tool on 2018-08-20 19:46:01.684977
-# This file is read by the 'jpyutil' module in order to load and configure the JVM from Python
-java_home = '/usr/lib/jvm/java-1.8.0-openjdk-amd64'
-jvm_dll = '/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/amd64/server/libjvm.so'
-jvm_maxmem = None
-jvm_classpath = []
-jvm_properties = {}
-jvm_options = []
