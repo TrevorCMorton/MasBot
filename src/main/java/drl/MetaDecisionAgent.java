d47d9369fe01000d67b4811430cd6ab9d89d4433
Trevor Morton <mortontc@rose-hulman.edu>
2019-01-11 12:24:15 -0500

    changed varios parameters and fixed range bug for replayer

--- src/main/java/drl/MetaDecisionAgent.java
+++ src/main/java/drl/MetaDecisionAgent.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MetaDecisionAgent {
-    public static final int size = 84;
+    public static final int size = 120;
 
     private ComputationGraph metaGraph;
     private AgentDependencyGraph dependencyGraph;
@@ -61,7 +61,7 @@ public class MetaDecisionAgent {
         ComputationGraphConfiguration.GraphBuilder builder = new NeuralNetConfiguration.Builder()
             .seed(123)
             .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-            .updater(new Adam(.0001))
+            .updater(new RmsProp(.00025))
             .graphBuilder();
 
         Collection<AgentDependencyGraph.Node> nodes = this.dependencyGraph.getNodes();
