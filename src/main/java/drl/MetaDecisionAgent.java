b606956d4951a51ef40ef6312ad7d88e63f87a36
Trevor Morton <mortontc@rose-hulman.edu>
2019-04-02 09:43:50 -0400

    Upping filter size

--- src/main/java/drl/MetaDecisionAgent.java
+++ src/main/java/drl/MetaDecisionAgent.java
@@ -289,11 +289,11 @@ public class MetaDecisionAgent {
         this.activations.add(activation2);
         this.activations.add(activation3);
 
-        int convOutSize = ((((MetaDecisionAgent.size - 8) / 4 + 1) - 4) / 2 + 1) - 2;
+        int convOutSize = ((((MetaDecisionAgent.size - 16) / 8 + 1) - 4) / 2 + 1) - 2;
 
         builder
                 .addLayer("Screen1",
-                        new ConvolutionLayer.Builder(8, 8).nIn(MetaDecisionAgent.depth).stride(4, 4).nOut(32).weightInit(WeightInit.XAVIER).activation(activation1).build(),
+                        new ConvolutionLayer.Builder(16, 16).nIn(MetaDecisionAgent.depth).stride(8, 8).nOut(32).weightInit(WeightInit.XAVIER).activation(activation1).build(),
                         "Screen")
                 .addLayer("Screen2",
                         new ConvolutionLayer.Builder(4, 4).stride(2, 2).nOut(64).weightInit(WeightInit.XAVIER).activation(activation2).build(),
